99afa51aa30248282bf6d86f8a98a28b086f54ff
gy0109 <1974326896@qq.com>
2019-04-25 20:34:30 +0800

    jd_book数据存不进去解决1

--- 11-爬取动态页面/jd/jd/spiders/jd_book.py
+++ 11-爬取动态页面/jd/jd/spiders/jd_book.py
@@ -25,13 +25,13 @@ class JdBookSpider(scrapy.Spider):
         #   p-price i
         #  p-name  em font
         gross = response.css('span#J_resCount::text').extract_first()
-        total = int(float(re.search('\d+\.\d+', gross).group()) * 10000)
+        total = int(float(re.search('\d+\.\d+', gross).group()) * 100)
         pageNum = total//60 + (1 if total % 60 else 0)
         for i in range(pageNum):
             url = '%s&page=%s' % (self.base_url, 2*i +1)
             yield SplashRequest(url, endpoint='execute',
                                 args={'lua_source': lua_script},
-                                cache_args=['lua_source'])
+                                cache_args=['lua_source'], callback=self.parse)
 
     def start_requests(self):
         # 第一个页面不需要js渲染
@@ -39,7 +39,8 @@ class JdBookSpider(scrapy.Spider):
 
     def parse(self, response):
         for sel in response.css('ul.gl-warp clearfix > li.gl.item'):
-            yield {
+            re_dict = {
                 'name': sel.css('div.p-name').xpath('string(.//em)').extract_fiest(),
                 'price': sel.css('div.p-price i::text').extract_first()
             }
+            yield Request(re_dict)
