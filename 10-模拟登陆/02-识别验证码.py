c108b2230a88bd75e4a10d9ca437a303f7a79832
gy0109 <1974326896@qq.com>
2019-04-25 14:26:04 +0800

    模拟登陆实现

--- 10-模拟登陆/02-识别验证码.py
+++ 10-模拟登陆/02-识别验证码.py
@@ -1,2 +1,12 @@
+"""
+1.OCR识别： 光学字符串  用于图片中图区文本信息  tesseract-ocr是利用该技术户实现的一个验证码识别库 pytesseract模块
 
+
+"""""
+from PIL import Image
+import pytesseract
+
+img = Image.open('./code.png')
+img = img.convert('L')
+print(pytesseract.image_to_string(img))
 
