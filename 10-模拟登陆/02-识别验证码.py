75ce320e4b16ec5cfc011387c199c17765d9efd6
gy0109 <1974326896@qq.com>
2019-04-25 17:13:39 +0800

    network识别和扔识别

--- 10-模拟登陆/02-识别验证码.py
+++ 10-模拟登陆/02-识别验证码.py
@@ -9,3 +9,14 @@ import pytesseract
 img = Image.open('./code1.png')
 img = img.convert('L')
 print(pytesseract.image_to_string(img))
+
+
+
+"""
+模拟登陆流程：
+1. 初始化登录信息
+2. 登录页面解析函数， 提取到输入用户名和密码的输入框，带入初始化信息，获取code图片，模拟登陆
+3. 判断是否登录成功函数： 判断error信息
+4. 解析code图片的函数: img.open(), img.conervt('L') pytesseract.image_to_string(img)  img.close() 文件打开要关闭的哦
+
+"""
--- 10-模拟登陆/logins/logins/spiders/logins.py
+++ 10-模拟登陆/logins/logins/spiders/logins.py
@@ -1,3 +1,4 @@
+import base64
 
 import scrapy
 import pytesseract
@@ -40,12 +41,14 @@ class CaptchalLoginSpider(scrapy.Spider):
             yield Request(login_response, callback=self.login, formdata=formdata, dont_filter=True)
 
     def get_captcha_by_OCR(self, data):
+        # OCR获取code的信息
         img = Image.open(BytesIO(data))
         img = img.convert('L')
         captcha = pytesseract.image_to_string(img)
         img.close()
         return captcha
 
+    # 判断登录是否成功函数
     def parse_login(self, response):
         info = json.loads(response.text)
         if info['error'] == '0':
@@ -55,4 +58,30 @@ class CaptchalLoginSpider(scrapy.Spider):
         logger.info('登录失败，请重新登录')
         return self.start_requests()
 
+    def get_captcha_by_network(self, data):
+        # 网络平台解析code
+        import requests
+        url = ''
+        appcode = ''
+        form = {}
+        form['convert_to_jpg'] = '0'   #  是否将图片转换为jpg形式
+        form['img_base64'] = base64.b64encode(data)       # 图片的数据形式转为64
+        form['typeId'] =  '3040'         # 验证码类型，代表几位汉字
 
+        headers = {}
+        response = requests.post(url, headers=headers, data=form)
+        res = response.json()
+
+        if res['showapi_res_code'] == 0:
+            return res['showapi_res_body']['Result']
+        return ''
+
+    def get_captcha_by_user(self, data):
+        # 人工识别
+        img = Image.open(BytesIO(data))
+        img.show()
+        captcha = input('请输入验证码：')
+        # img = img.convert('L')
+        # pytesseract.image_to_string(img)
+        img.close()
+        return captcha
